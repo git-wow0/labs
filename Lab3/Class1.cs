8ba228334d854540b734e2e7cfb2fa9d918e4f74
Елизавета Шевченко <lizochkashevchenko_31@mail.ru>
2019-11-21 18:58:00 +0300

    yeah

---
+++ Lab3/Class1.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_3
+{
+    class Vector
+    {
+        public const String labName = "Лабороторная работа №3";
+
+        static int VectorCount = 0;
+        int number;
+        int status;
+        string some, thing;
+            string total;
+        public static int Count { get; private set; } = 0;
+       
+        int[] array;
+        int Length = 0;
+        
+        public readonly string myName = " Шевченко Елизавета";
+        Random rnd = new Random();
+
+         private Vector( string some, string thing )
+        {
+            some = "some";
+            thing = "thing";
+            total = some + thing;
+        }
+
+
+        static Vector()
+        {
+            Count++;
+            
+        }
+        
+       
+        public Vector()
+        {
+            array = new int[10];
+            Length = 10;
+            for (int count = 0; count < Length; count++)
+                array[count] = (rnd.Next(-10, 15));
+            VectorCount++;
+        }
+
+        public Vector(int size)
+        {
+            array = new int[size];
+            Length = size;
+            for (int count = 0; count < Length; count++)
+                array[count] = (rnd.Next(-10,15));
+            VectorCount++;
+        }
+        
+
+        public Vector(int a, int b, int c, int d, int size)
+        {
+            
+                array = new int[] { a, b, c, d, size };
+                Length = size;
+                VectorCount++;
+
+                
+            
+        }
+        
+
+        public int this[int i]
+        {
+            get
+            {
+                if (i < 0 && i > Length)
+                {
+                    Console.WriteLine("Пpоверьте диапазон элементов массива");
+                    status = 100;
+                    return status;
+                }
+                return array[i];
+            }
+            set
+            {
+                if (i >= 0 && i <= Length)
+                {
+
+                    if (value <= 15 & value > -10)
+                        array[i] = value;
+                    else
+                    {
+                        Console.WriteLine("Пpоверьте введённое значение!");
+                        status = -100;
+                    }
+                }
+                else
+                    Console.WriteLine("Пpоверьте диапазон элементов массива!");
+                status = 100;
+            }
+        }
+
+        public void PrintArray()
+        {
+            
+            for (int count = 0; count < this.Length; count++)
+                Console.Write(array[count] + " ");
+        }
+
+        public void SumArray(int number)
+        {
+
+           
+            for (int i = 0; i < this.Length; i++)
+                array[i] += number;
+            
+            
+            
+        }
+
+         public void MulpiplyArray(int number)
+          {
+           
+              for (int i = 0; i < this.Length; i++)
+                  array[i]*=number;
+            
+
+        }
+
+        public bool IsNullInArray()
+        {
+            for (int i = 0; i < this.Length; i++)
+            {
+                if (array[i] == 0)
+                    return true;
+            }
+            return false;
+        }
+        public int GetMin()
+        {
+            return array.Min();
+        }
+        public static int GetCount()
+        {
+            return VectorCount;
+        }
+    }
+
+
+   
+
+    partial class vector0
+    {
+        public string name { get; private set; }
+        public int numb { get; private set; }
+
+        public override bool Equals(object obj)
+        {
+           
+            if (obj.GetType() != this.GetType())
+                return false;
+            vector0 arr1 = (vector0)obj;
+            return (this.name == arr1.name && this.numb == arr1.numb);
+        }
+
+        public override int GetHashCode()
+        {
+           return name.GetHashCode();
+            
+        }
+    }
+
+    partial class vector0
+    {
+
+        public vector0(string Name, int Numb)
+        {
+            this.name = Name;
+            this.numb = Numb;
+
+        }
+
+
+
+
+
+    }
+}
--- Lab3/Program.cs
+++ Lab3/Program.cs
@@ -1,12 +1,102 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-namespace Lab3
+namespace OOP_3
 {
     class Program
     {
+        public static void Sum(int A, int B, out int sum)
+        {
+            sum = A + B;
+        }
+
+        public static void Change(int _c, ref int _d)
+        {
+            _d += _c;
+        }
+
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Статик переменная сейчас: " + Vector.Count);
+            Console.WriteLine(Vector.labName);
+           
+           
+            Vector v1 = new Vector();
+            v1.PrintArray();
+            Console.WriteLine(v1.GetType());
+            Console.WriteLine("\n Введите значение переменной");
+
+           int number= Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("\n Сумма : "); v1.SumArray(number); v1.PrintArray();
+            Console.WriteLine(" Произведение : "); v1.MulpiplyArray(number); v1.PrintArray();
+            Console.WriteLine("\n--------------------------------");
+            
+            Vector v2 = new Vector(8);
+            v2.PrintArray();
+            Console.WriteLine("\n Сумма : "); v2.SumArray(number); v2.PrintArray();
+            Console.WriteLine(" Произведение :"); v2.MulpiplyArray(number); v2.PrintArray();
+            Console.WriteLine("\n--------------------------------");
+
+            Vector v3 = new Vector(43, -5, 1, 90, 4);
+            v3.PrintArray();
+            Console.WriteLine("\n Сумма :"); v3.SumArray(number); v3.PrintArray();
+            Console.WriteLine(" Произведение :"); v3.MulpiplyArray(number); v3.PrintArray();
+            Console.WriteLine("\n--------------------------------\n");
+
+            Console.WriteLine("А сейчас она не изменилась: " + Vector.Count);
+            Console.WriteLine("Всего объектов: " + Vector.GetCount());
+
+            Vector[] vectors = { v1, v2, v3 };
+
+            for (int i = 0; i < vectors.Length; i++)
+            {
+                if (vectors[i].IsNullInArray())
+                    Console.WriteLine("В массиве {0}-го объекта есть ноль! ", (i + 1));
+            }
+
+            for (int i = 0; i < vectors.Length; i++)
+                Console.WriteLine("Минимальный элемент {0} в массиве: {1}", i + 1, vectors[i].GetMin());
+            Console.WriteLine("\n--------------------------------\n");
+
+            int a, b, c, d, sum;
+            a = 5;
+            b = 10;
+            c = 20;
+            d = 25;
+            Console.WriteLine("a = {0}\nb = {1}\nc = {2}\nd = {3}", a, b, c, d);
+            Sum(a, b, out sum);
+            Console.WriteLine("Сумма a и b равна: " + sum);
+            Change(c, ref d);
+            Console.WriteLine("После работы с ref: ");
+            Console.WriteLine("\nc = {0} \nd = {1}", c, d);
+            Console.WriteLine("\n--------------------------------\n");
+
+
+            vector0 Vector1 = new vector0("Vector1", 10);
+            vector0 Vector2 = new vector0("Vector2", 20);
+
+            Console.WriteLine("\nназвание: {0}", Vector1.name);
+            Console.WriteLine("\nколичество: {0}", Vector1.numb);
+            Console.WriteLine("Хеш-код: {0}", Vector1.GetHashCode());
+
+            Console.WriteLine("\nназвание: {0}", Vector2.name);
+            Console.WriteLine("\nколичество: {0}", Vector2.numb);
+            Console.WriteLine("Хеш-код: {0}", Vector2.GetHashCode());
+            Console.WriteLine(Vector2.Equals(Vector1) ? " \nЭти объекты одинаковы\n" : "\nЭти объекты НЕ одинаковы\n");
+            Console.WriteLine("Тип объекта Vector1: {0}", Vector1.GetType());
+            Console.WriteLine("\n--------------------------------\n");
+            var anon = new { x = 5, Name = "vectorX" };
+            Console.WriteLine(anon.GetType());
+            Console.ReadKey();
+
+
         }
+
+
     }
 }
+
